d862e295bab61771582536726b9f3b554ae8b3da
FaithOzoanieke <faithozoanieke@gmail.com>
2025-08-14 15:58:18 +0100

    dashboard

--- app/routes.ts
+++ app/routes.ts
@@ -7,19 +7,3 @@ import {
 export default [
   route('dashboard', './routes/admin/dashboard.tsx'),
 ] satisfies RouteConfig;
-
-
-// import {
-//   type RouteConfig,
-//   route,
-//   index,
-// } from "@react-router/dev/routes";
-
-// export default [
-//   // parent route
-//   route("dashboard", "./routes/admin/dashboard.tsx", [
-//     // child routes
-//     // index("./home.tsx"),
-//     // route("settings", "./settings.tsx"),
-//   ]),
-// ] satisfies RouteConfig;
